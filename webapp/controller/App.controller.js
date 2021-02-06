4c40a41aaf1b7aaf53e4e2f0899a8b11baa4bc25
subin <subinsamrat96@gmail.com>
2021-02-06 15:54:58 +0000

    step-7 JSON Model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,33 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
-  return Controller.extend("com.ui5walkthrough.controller.App", {
-    onPress: function () {
-      // show a native JavaScript alert
-      alert("Hello World");
-    },
-  });
-});
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (Controller, MessageToast, JSONModel) {
+    "use strict";
+    return Controller.extend("com.ui5walkthrough.controller.App", {
+
+      onInit: function() {
+        var json = new JSONModel();
+
+        var data = {
+          introMessage: {
+            name: "World"
+          }
+        };
+
+        json.setData(data);
+        // default model
+        this.getView().setModel(json);
+
+        // named model
+        // this.getView().setModel({json:"newModel"})
+      },
+
+      onPress: function () {
+        MessageToast.show("This is a message toast");
+      },
+    });
+  }
+);
