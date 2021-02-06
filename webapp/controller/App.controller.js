521ffbff9f84ae2753f9d3b077c78809b7d76092
subin <subinsamrat96@gmail.com>
2021-02-06 17:37:18 +0000

    step-8 translatable texts

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,18 +3,18 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
   ],
-  function (Controller, MessageToast, JSONModel) {
+  function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("com.ui5walkthrough.controller.App", {
-
-      onInit: function() {
+      onInit: function () {
         var json = new JSONModel();
 
         var data = {
           introMessage: {
-            name: "World"
-          }
+            name: "World",
+          },
         };
 
         json.setData(data);
@@ -23,10 +23,24 @@ sap.ui.define(
 
         // named model
         // this.getView().setModel({json:"newModel"})
+
+        // set i18n model to view
+        var i18nModel = new ResourceModel({
+          bundleName:"com.ui5walkthrough.i18n.i18n",
+          supportedLocales:[""],
+          fallbackLocales:""
+        });
+
+        this.getView().setModel(i18nModel, "i18n");
+
       },
 
       onPress: function () {
-        MessageToast.show("This is a message toast");
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var oText = this.getView().getModel().getProperty("/introMessage/name");
+        var oMsg = oBundle.getText("inputDescription", oText);
+
+        MessageToast.show(oMsg);
       },
     });
   }
